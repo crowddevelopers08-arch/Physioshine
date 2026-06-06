bd2999cf0bc26cedc2853b611811159e830c652f
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-06-06 15:17:33 +0530

    telessss

---
+++ src/app/testing/page.tsx
@@ -0,0 +1,36 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import USPStrip from "@/components/USPStrip";
+import ClinicSection from "@/components/ClinicSection";
+import PathToRecovery from "@/components/PathToRecovery";
+import TreatmentGrid from "@/components/TreatmentGrid";
+import DoctorsSection from "@/components/DoctorsSection";
+import VideoTestimonials from "@/components/VideoTestimonials";
+import ReviewsCarousel from "@/components/ReviewsCarousel";
+import FAQSection from "@/components/FAQSection";
+import FinalCTA from "@/components/FinalCTA";
+import Footer from "@/components/Footer";
+import ThreeHomepageBackground from "@/components/ThreeHomepageBackground";
+import TestingHeroSection from "@/components/testionherosection";
+
+export default function Home() {
+  return (
+    <div className="relative bg-surface">
+      <ThreeHomepageBackground />
+      <div className="relative z-10">
+        <Navbar />
+        <TestingHeroSection />
+        <USPStrip />
+        <ClinicSection />
+        <PathToRecovery />
+        <TreatmentGrid />
+        <DoctorsSection />
+        <VideoTestimonials />
+        <ReviewsCarousel />
+        <FAQSection />
+        {/* <FinalCTA /> */}
+        <Footer />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/testing.tsx
@@ -0,0 +1,13 @@
+export default function ZohoBookingForm() {
+  return (
+    <iframe
+      src="https://physioshine.zohobookings.in/portal-embed#/445097000000034048"
+      style={{ border: "none" }}
+      width="100%"
+      height="600"
+      title="Book an Appointment – Physio Shine"
+      loading="lazy"
+      allow="payment"
+    />
+  );
+}
